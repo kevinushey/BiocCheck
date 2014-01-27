decf3680af9e1998210beb5ebb606336551a147e
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-01-26 22:53:05 -0800

    cosmetic

--- inst/testpackages/testpkg0/R/requireme.R
+++ inst/testpackages/testpkg0/R/requireme.R
@@ -9,3 +9,8 @@ require(package=testpkg0)
 require(package = teskpkg0)
 require('testpkg0')
 library('testpkg0')
+require(lib.loc=NULL, package=testpkg0)
+#bad:
+require(help=testpkg0)
+require(lib.loc=NULL, help = 
+testpkg0)
